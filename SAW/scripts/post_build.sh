d2039ff179c84ad409a002bc5ebbec365eaa948b
Samuel Breese <samuel@chame.co>
2020-12-07 12:44:10 -0500

    Examine LLVM bitcode

--- SAW/scripts/post_build.sh
+++ SAW/scripts/post_build.sh
@@ -10,3 +10,4 @@ mkdir -p build/llvm/crypto build/x86/crypto
 cp build_src/llvm/crypto/crypto_test build/llvm/crypto/crypto_test
 cp build_src/x86/crypto/crypto_test build/x86/crypto/crypto_test
 extract-bc build/llvm/crypto/crypto_test
+ls -lah build/llvm/crypto/crypto_test.bc
